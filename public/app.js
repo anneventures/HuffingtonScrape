bb6606aa0ea569215b669ccdb26ccb0a06234d32
Annecodes <info.annesimon@gmail.com>
2018-10-08 15:19:37 -0400

    properly linked news article URL

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<a href='http://www.huffingtonpost.ca" + data[i].link + "' target='_blank'>" + " (Visit this news article)" + "</a></p>");
   }
 });
 
